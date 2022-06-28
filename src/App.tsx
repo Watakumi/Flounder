2d06c93e69ef2ed14d1030b018b12f593acdc3b8
石渡拓海 <t-ishiwata@sight-visit.com>
2022-06-28 23:05:30 +0900

    Display Mermaid

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,68 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
-import { Container, TextField, Typography } from '@mui/material';
+import { Button, Container, Input, TextField, Typography } from '@mui/material';
 import mermaid from 'mermaid';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+
+type Inputs = {
+  example: string;
+};
+
+const graphText = (target: string) => {
+  return `
+graph LR \n
+A --- ${target} \n
+${target}-->C[fa:fa-ban forbidden] \n
+${target}-->D(fa:fa-spinner) \n
+;
+`;
+};
+
+const Mermaid = ({ target }: { target: string }) => {
+  return `<div class="mermaid">${graphText(target)}</div>`;
+};
 
 function App() {
-  useEffect(() => {
-    mermaid.initialize({
-      startOnLoad: true,
-    });
-  });
+  const [target, setTarget] = useState('B');
+  const mermaidElm = useRef<HTMLDivElement>(null);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  const example = `
-  graph LR
-  A --- B
-  B-->C[fa:fa-ban forbidden]
-  B-->D(fa:fa-spinner)
-  `;
+  const onSubmit: SubmitHandler<Inputs> = (data) => setTarget(data.example);
+
+  useEffect(() => {
+    const elm = mermaidElm.current;
+    if (!elm) return;
+    elm.innerHTML = Mermaid({ target: target });
+    mermaid.init('.mermaid');
+  }, [target]);
+
+  const outputs = graphText(target).split('\n');
 
   return (
     <Container fixed>
       <Typography variant="h2" component="h1">
         Welcome to <span style={{ fontWeight: 'bold' }}>Flounder!</span>
       </Typography>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <div className="mermaid">{example}</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="example"
+          control={control}
+          defaultValue=""
+          render={({ field }) => <TextField {...field} />}
+        />
+
+        <Button type="submit" variant="contained">
+          Submit
+        </Button>
+      </form>
+      <div ref={mermaidElm}></div>
+      {outputs.map((output) => (
+        <h5>{output}</h5>
+      ))}
     </Container>
   );
 }
