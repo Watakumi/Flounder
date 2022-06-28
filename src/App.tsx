458f1f0b14f4b4e494a68ad1614ca2bf1251456f
石渡拓海 <t-ishiwata@sight-visit.com>
2022-06-28 22:17:32 +0900

    test mermaid

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
-import { Container, Typography } from '@mui/material';
+import { Container, TextField, Typography } from '@mui/material';
+import mermaid from 'mermaid';
 
 function App() {
+  useEffect(() => {
+    mermaid.initialize({
+      startOnLoad: true,
+    });
+  });
+
+  const example = `
+  graph LR
+  A --- B
+  B-->C[fa:fa-ban forbidden]
+  B-->D(fa:fa-spinner)
+  `;
+
   return (
     <Container fixed>
       <Typography variant="h2" component="h1">
         Welcome to <span style={{ fontWeight: 'bold' }}>Flounder!</span>
       </Typography>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <div className="mermaid">{example}</div>
     </Container>
   );
 }
