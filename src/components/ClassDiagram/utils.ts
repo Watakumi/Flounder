cf9acd534edddacd66bdd92dbbcbb00f02a8987b
石渡拓海 <t-ishiwata@sight-visit.com>
2022-07-03 23:46:54 +0900

    Add Comment to relationships

--- src/components/ClassDiagram/utils.ts
+++ src/components/ClassDiagram/utils.ts
@@ -1,12 +1,12 @@
 export const relationships = {
-  inheritence: '<|--',
-  composition: '*--',
-  aggregation: 'o--',
-  association: '-->',
-  linkSolid: '--',
-  linkDashed: '..',
-  dependency: '..>',
-  realization: '..|>',
+  inheritence: ['<|--', '継承'],
+  composition: ['*--', '委譲'],
+  aggregation: ['o--', '集約'],
+  association: ['-->', '関連'],
+  linkSolid: ['--', '線'],
+  linkDashed: ['..', '破線'],
+  dependency: ['..>', '依存'],
+  realization: ['..|>', '実現'],
 };
 
 export const visibility = {
