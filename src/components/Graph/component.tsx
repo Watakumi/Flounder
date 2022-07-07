e231750179ab5f9253e94564f0cfb397b0ae7264
石渡拓海 <t-ishiwata@sight-visit.com>
2022-07-07 23:27:16 +0900

    コピペできるようにする

--- src/components/Graph/component.tsx
+++ src/components/Graph/component.tsx
@@ -1,6 +1,13 @@
 import { Box, Typography } from '@mui/material';
+import { useRef, useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 export function Code({ outputs }: { outputs: Array<string> }) {
+  const [copied, setCopied] = useState(false);
+  const startText = '```mermaid \n';
+  const endText = '\n ```';
+  const rowCode = `${startText}${outputs.join('\n')}${endText}`;
+  const mermaidText = useRef<HTMLHeadingElement>(null);
   return (
     <Box
       sx={{
@@ -9,15 +16,23 @@ export function Code({ outputs }: { outputs: Array<string> }) {
         border: 2,
         borderColor: 'gray',
         borderRadius: '16px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
       <Typography variant="h3">Code</Typography>
-      <Box>
-        <h5>```mermaid</h5>
-        {outputs.map((output, index) => (
-          <h5 key={index}>{output}</h5>
-        ))}
-        <h5>```</h5>
+
+      <CopyToClipboard text={rowCode}>
+        <button>Copy to clipboard</button>
+      </CopyToClipboard>
+
+      <Box sx={{ textAlign: 'left', whiteSpace: 'pre-wrap' }}>
+        <h5>
+          {rowCode}
+          ```
+        </h5>
       </Box>
     </Box>
   );
